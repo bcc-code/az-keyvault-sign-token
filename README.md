212dbebbc50a430b1423ad1e2017d528731234d0
Adelin Niculae <43118261+adelinn@users.noreply.github.com>
2024-09-30 15:53:40 +0200

    code

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# az-keyvault-sign-token
-GitHub Action for signing a GitHub App token using a key stored in Azure Key Vault
+# Sign GitHub App token with Azure action
+
+This action allows signing a GitHub App token using a key stored in Azure Key Vault.
+
+## Usage
+
+### Pre-requisites
+
+* Azure subscription
+* GitHub App
+* Key Vault
+
+Create a private key and upload it to Azure Key Vault.
+
+### Inputs
+
+* `gh-app-client-id` - The Client ID of the GitHub App to sign the token for.
+* `key-vault-name` - The name of the Azure Key Vault resource that holds the GitHub App private key.
+* `key-name` - The name of the key in the Azure Key Vault resource that holds the GitHub App private key.
+* `token-duration` - The duration in seconds for which the token will be valid. Default: `300`
+
+### Outputs
+
+* `gh_token` - The resulting GitHub token, signed with the key stored in Azure Key Vault.
+
+### Example workflow
+
+```yaml
+name: Sign GitHub App token with Azure
+
+on: push
+
+permissions:
+  id-token: write
+  contents: read
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Azure login
+      uses: azure/login@v2
+      with:
+        client-id: ${{ secrets.AZURE_CLIENT_ID }}
+        tenant-id: ${{ secrets.AZURE_TENANT_ID }}
+        subscription-id: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+
+    - name: Sign GitHub App token with Azure
+      uses: bcc-code/az-keyvault-sign-token@v1
+      with:
+        gh-app-client-id: ${{ vars.GITHUB_APP_CLIENT_ID }}
+        key-vault-name: ${{ vars.AZURE_KEY_VAULT_NAME }}
+        key-name: 'gh-app-pem'
+```
+
+## License
+
+The scripts and documentation in this project are released under the [Apache License 2.0](LICENSE)
---
+++ index.js
@@ -0,0 +1,54 @@
+const core = require('@actions/core');
+const { base64encodeJSON } = require('./utils');
+const { createHash } = require('crypto');
+const { DefaultAzureCredential } = require('@azure/identity');
+const {
+  CryptographyClient,
+  KeyClient,
+  KnownSignatureAlgorithms
+} = require('@azure/keyvault-keys');
+
+try {
+  const credential = new DefaultAzureCredential();
+
+  const githubAppClientId = core.getInput('gh-app-client-id');
+  const keyVaultName = core.getInput('key-vault-name');
+  const keyName = core.getInput('key-name');
+  const tokenDuration = parseInt(core.getInput('token-duration'), 10);
+
+  const now = Math.floor(Date.now() / 1000);
+  const nowWithSafetyMargin = now - 30;
+  const expiration = now + tokenDuration;
+
+  const jwtHeader = base64encodeJSON({
+    alg: 'RS256',
+    typ: 'JWT'
+  });
+
+  const jwtPayload = base64encodeJSON({
+    iss: githubAppClientId,
+    iat: nowWithSafetyMargin,
+    exp: expiration
+  });
+
+  const jwtData = `${jwtHeader}.${jwtPayload}`;
+  const jwtDataDigest = createHash('sha256').update(jwtData).digest();
+
+  const serviceClient = new KeyClient(
+    `https://${keyVaultName}.vault.azure.net`,
+    credential
+  );
+  const keyVaultKey = await serviceClient.getKey(keyName);
+
+  if (keyVaultKey?.name) {
+    const cryptoClient = new CryptographyClient(keyVaultKey, credential);
+    const { result: signature } = await cryptoClient.sign(KnownSignatureAlgorithms.RS256, jwtDataDigest);
+    const jwtSignature = urlBase64encode(signature);
+
+    const ghToken = `${jwtData}.${jwtSignature}`;
+    core.setSecret(ghToken);
+    core.setOutput('gh_token', ghToken);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
